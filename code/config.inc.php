0ec10ec9be08726e596c10c312a70be36afdffe7
root <root@sys.ignisband.ro>
2024-06-18 16:37:04 +0300

    update boost and jquery

---
+++ code/config.inc.php
@@ -0,0 +1,10 @@
+<?php
+define('SMARTY_DIR', '/usr/share/php/smarty3/');
+$ldapuri = "ldap://localhost";
+$basedn ="dc=machinet";
+$admindn ="cn=Manager,dc=machinet";
+$rootdn="cn=admin,dc=config";
+//$domain ="machinet.ro";
+$error_code="ZERO";
+
+?>
